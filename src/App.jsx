88ba8cd85bb15f0725d73b659d358992cfe4a279
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-14 11:06:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './components/Countries/countries'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>React World Tour</h2>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
